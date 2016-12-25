e669fc8beb06f01ad3f2eac15931b22d44f6e191
Sreejith Rajeevan <r.sreejithtoo@gmail.com>
2016-12-24 20:52:52 -0600

    Day 3
    Generics Demo

--- Program.cs
+++ Program.cs
@@ -1,38 +1,27 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day3_HashTableandArrayList
+namespace Day3_Generics
 {
+    class Generic<T>
+    {
+        public T Gvariable1;
+        public T Gmethod(T gvar2)
+        {
+            Gvariable1 = gvar2;
+            return Gvariable1;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Hashtable ht = new Hashtable();
-            ht.Add(1, "one");
-            ht.Add(2, "Two");
-            ht.Add(3, "Three");
-            ht.Add(4, "Four");
-            ht.Add(5, "Five");
-            foreach(DictionaryEntry x in ht)
-            {
-                Console.WriteLine("key{0} and value:{1}",x.Key,x.Value);
-            }
-            ArrayList at = new ArrayList();
-            Console.WriteLine("array list values");
-            at.Add(0);
-            at.Add(1);
-            at.Add(2);
-            at.Add(3);
-            at.Add(4);
-            foreach(int i in at)
-            {
-                Console.WriteLine(i);
-            }
-
-
+            Generic<string> gobj = new Generic<string>();
+            String msg=gobj.Gmethod("Sreejith");
+            Console.WriteLine("Hello " + msg);
         }
     }
 }
