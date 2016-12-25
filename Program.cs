03a513bbc9a5239b3cee07b4009d9af562869c88
Sreejith Rajeevan <r.sreejithtoo@gmail.com>
2016-12-24 20:53:53 -0600

    Day 3
    Array List Demo

--- Program.cs
+++ Program.cs
@@ -1,30 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//Demo of Array,ArrayList,Foreach and While
+//Arrays are of fixed length
+//ArrayList is flexible array with list of objects
+using System;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day3_BoxingandUnboxing
+namespace Day3_Arrayist
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i = 10;
-            object o = i;//Boxing
-            Console.WriteLine("Boxed output:"+o);
+            ArrayList arr = new ArrayList();//Array,Array List,Foreach and While
+            arr.Add(0);
+            arr.Add(1);
+            arr.Add(2);
+            arr.Add(3);
+            Console.WriteLine("Using Foreach loop");
+            foreach (int li in arr)
+            {
+                Console.WriteLine(li);
+            }
+            int i = 0;
+            Console.WriteLine(arr.Count);
+            Console.WriteLine("Using While Looop");
+            while (i < arr.Count)
+            {
 
-            int j = (int)o;
-            Console.WriteLine("Unboxed Output:" + j);
-            Console.WriteLine("Demo of String and String Builder");
-            string str = "Hello";
-            Console.WriteLine(str);
-            str = "world";
-            Console.WriteLine(str);
-            StringBuilder stb = new StringBuilder("Hell0");
-            stb.Append("world");
-            Console.WriteLine(stb);
-
+                Console.WriteLine(i);
+                i++;
+            }
         }
     }
 }
+
