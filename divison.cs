4d1a6cf2dd3c69de859a72a008054f2685fc0c9e
Sreejith Rajeevan <r.sreejithtoo@gmail.com>
2016-12-28 22:39:45 -0600

    Day5_Exception_Handling

---
+++ divison.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_exception
+{
+    class Divison
+    {
+        int answer;
+        public void divideNumbers(int a,int b)
+        {
+            try
+            {
+                answer = a / b;
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("Excaption :"+e);
+            }
+            finally
+            {
+                Console.WriteLine("Answer is "+answer);
+            }
+            
+        }
+        static void Main()
+        {
+            Divison dm = new Divison();
+            dm.divideNumbers(10,0);
+        }
+    }
+}
